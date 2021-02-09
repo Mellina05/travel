0642d4a04a27bd15a0dddd7270a96d4621b1bb69
Mellina05 <meidipeng2018@u.northwestern.edu>
2021-02-09 13:54:21 -0800

    :construction: Add auth provider

---
+++ src/screens/PlanPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+/**
+ * CreatePlanPage is the creation page of a plan.
+ */
+
+export function PlanPage({ navigation }) {
+
+    return (
+        <View>
+            <Text>Plans:</Text>
+        </View>
+    );
+}
